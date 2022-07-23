fed8cd3c3bbc7158bcca6fd312e461ec6dc4abcd
vbthang <bathangvu@gmail.com>
2022-07-23 09:41:40 +0700

    add function getInfo

--- sign-up/sign-up-main.js
+++ sign-up/sign-up-main.js
@@ -1,16 +1,25 @@
-const userName = document.getElementById('user')
-const email = document.getElementById('email')
-const password = document.getElementById('pass')
-const password_repeat = document.getElementById('pass-repeat')
 const signupBtn = document.querySelector('.form-submit')
 
-
-signupBtn.addEventListener('click', function() {
-  const user = {
-    'password': password.value,
-    'email': email.value
+function getInfo(e) {
+  const userName = document.getElementById('user')
+  const email = document.getElementById('email')
+  const password = document.getElementById('pass')
+  const password_confirm = document.getElementById('pass-confirm')
+  
+  let user = {
+    username : userName.value,
+    email : email.value,
+    password : password.value
   }
-  localStorage.setItem(userName.value, JSON.stringify(user))
-  console.log('Done');
-})
+  let json = JSON.stringify(user)
+  localStorage.setItem(userName.value, json)
+  alert('Đăng kí thành công')
+  // reset form
+  userName.value = ''
+  email.value = ''
+  password.value = ''
+  password_confirm.value = ''
+}
 
+
+signupBtn.addEventListener('click', getInfo)
